eeeca14ee6f0896a1bb4517808748904365488f2
336098 <33336196+336098@users.noreply.github.com>
2020-11-04 13:43:10 -0600

    Adds pathfinding scripts
    Changes the enemy script so that it takes in a list of game objects to determine shortest position, paths towards said position, and rotates to face the base

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,19 +6,30 @@ public class Enemy : MonoBehaviour
 {
     float health = 50f;
 
+    public GameObject pointCollection;
     public Transform cabana;
-    float rotationSpeed = 5f;
+    float moveSpeed = 5f;
+    float rotationSpeed = 3f;
+
+    GameObject closestObj;
+    Vector3 direction;
+    Quaternion lookRotation;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        DeterminePosition();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveToPosition();
+
+        if (transform.position == closestObj.transform.position)
+        {
+            LookAtCabana();
+        }
     }
 
     public void TakeDamage(float amount)
@@ -33,5 +44,36 @@ public class Enemy : MonoBehaviour
     void Die()
     {
         Destroy(gameObject);
+    }
+
+    void MoveToPosition()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, closestObj.transform.position, moveSpeed * Time.deltaTime);
+    }
+
+    void LookAtCabana()
+    {
+        direction = (cabana.position - transform.position).normalized;
+        lookRotation = Quaternion.LookRotation(direction);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotationSpeed * Time.deltaTime);
+    }
+
+    void DeterminePosition()
+    {
+        List<GameObject> objectList = pointCollection.GetComponent<ShootPointList>().GetObjectList();
+
+        float distance;
+        float shortestDistance = Mathf.Infinity;
+
+        for (int i = 0; i < objectList.Count; i++)
+        {
+            distance = Vector3.Distance(transform.position, objectList[i].transform.position);
+            if (distance < shortestDistance)
+            {
+                shortestDistance = distance;
+                closestObj = objectList[i];
+            }
+        }
     }
 }
---
+++ Assets/Scripts/ShootPointList.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootPointList : MonoBehaviour
+{
+    public List<GameObject> objectList;
+
+    public GameObject shootPoint1;
+    public GameObject shootPoint2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        objectList.Add(shootPoint1);
+        objectList.Add(shootPoint2);
+    }
+
+    public List<GameObject> GetObjectList()
+    {
+        return objectList;
+    }
+}
